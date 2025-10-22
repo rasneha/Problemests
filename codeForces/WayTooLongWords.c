a09daff30db30a03fc791d1b18d819c13b8e477e
Raisa Alam <raisaalamsneha@gmail.com>
2025-10-23 02:47:52 +0600

    sol of Way to long Words

---
+++ codeForces/WayTooLongWords.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+   
+    scanf("%d", &n);
+    char word[101];
+    for (int i = 0; i < n; i++) {
+        scanf("%s", word);
+        int length = 0;
+        while (word[length] != '\0') {
+            length++;
+        }
+
+       if (length > 10) {
+            printf("%c%d%c\n", word[0], length - 2, word[length - 1]);
+        } else {
+            printf("%s\n", word);
+        } 
+    }
+    return 0;
+}
