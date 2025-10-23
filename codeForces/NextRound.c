826d2160c6333f8242c99cf5d18422bcd9cc8cd5
Raisa Alam <raisaalamsneha@gmail.com>
2025-10-23 22:09:08 +0600

    sol of Next round

---
+++ codeForces/NextRound.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int scores[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &scores[i]);
+    }
+    int win= scores[k - 1];
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (scores[i] > 0 && scores[i] >= win) {
+            count++;
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}
