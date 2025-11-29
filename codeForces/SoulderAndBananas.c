0395fa75cf19dac9fab93531ca09dcf83c73676f
Raisa Alam <raisaalamsneha@gmail.com>
2025-11-29 20:50:01 +0600

    soldier and bananas

---
+++ codeForces/SoulderAndBananas.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+int k,n,w;
+scanf("%d %d %d", &k, &n, &w);
+int total = 0;
+for(int i = 1; i <= w; i++){
+    total += i*k;
+}
+if(total > n){
+    printf("%d", total - n);
+}
+else{
+    printf("0");
+}
+    return 0;
+}
